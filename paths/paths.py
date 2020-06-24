2c6adfd01fdbb8fd5cf790975ce9f0255097365f
josephus <gwang.jin.kim.phd@gmail.com>
2020-06-24 08:01:10 +0200

    added paths

---
+++ paths/paths.py
@@ -0,0 +1,71 @@
+############################
+# organize the paths
+############################
+
+import os
+
+bases = {"ss": "/media/daten/arnold/josephus",
+         "super": "/home/josephus/Downloads",
+         "work": "/media/josephus/My Book",
+         "backup": "/media/josephus/mybook"}
+
+ss = {
+    "base": bases['ss'],
+    "galaxy": os.path.join(bases['ss'], 'data', 'galaxy_2019_Nov'),
+    "model": os.path.join(bases['ss'], 'model'),
+    "genome": os.path.join(bases['ss'], 'genome'),
+    "results": os.path.join(bases['ss'], 'data', 'galaxy_2019_Nov', 'results')
+}
+
+sup = {
+    "base": bases['super'],
+    "galaxy": os.path.join(bases['super'], 'data', 'galaxy_2019_Nov'),
+    "model": os.path.join(bases['super'], 'model'),
+    "genome": os.path.join(bases['super'], 'genome'),
+    "results": os.path.join(bases['super'], 'data', 'galaxy_2019_Nov', 'results')
+}
+
+work = {
+    "base": bases['work'],
+    "archive": "/media/josephus/archive_big",
+    "galaxy": os.path.join(bases['work'], 'galaxy_2019_Nov'),
+    "model": os.path.join(bases['work'], 'model'),
+    "genome": os.path.join(bases['work'], 'genome'),
+    "results": os.path.join(bases['work'], 'galaxy_2019_Nov', 'results')
+}
+
+backup = {
+    "base": bases['backup'],
+    "galaxy": os.path.join(bases['backup'], 'data', 'galaxy_2019_Nov'),
+    "model": os.path.join(bases['backup'], 'model'),
+    "genome": os.path.join(bases['backup'], 'genome'),
+    "results": os.path.join(bases['backup'], 'data', 'galaxy_2019_Nov', 'results')
+}
+
+paths = {"ss": ss,
+         "super": sup,
+         "work": work,
+         "backup": backup}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="paths",
+    version="0.0.1",
+    description="my paths",
+    packages=["paths"],
+    author="Gwang-Jin Kim",
+    author_email="gwang.jin.kim.phd@gmail.com",
+    keywords=["paths", "bioinformatics"],
+    url="https://bitbucket.org/gwangjinkim/paths"
+)
